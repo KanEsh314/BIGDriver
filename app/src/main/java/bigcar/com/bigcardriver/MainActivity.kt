a4ad414b89662b299ff95008cd2f852c88a12cf5
wan muzaffar Wan Hashim <wanmuz86@gmail.com>
2018-05-23 16:28:09 +0800

    push notif

--- app/src/main/java/bigcar/com/bigcardriver/MainActivity.kt
+++ app/src/main/java/bigcar/com/bigcardriver/MainActivity.kt
@@ -1,5 +1,6 @@
 package bigcar.com.bigcardriver
 
+import android.content.Context
 import android.graphics.PorterDuff
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -9,8 +10,21 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
+import android.text.Editable
+import android.util.Log
 import android.view.MenuItem
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.firebase.iid.FirebaseInstanceId
+
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -46,11 +60,66 @@ class MainActivity : AppCompatActivity() {
             }
         })
         setupTabIcons()
+
+        val intent = intent
+        val message = intent.getStringExtra("message")
+        Log.d("debug","message is "+message)
+        if(message != null) {
+            Log.d("debug","message.. I m here")
+            AlertDialog.Builder(this)
+                    .setTitle("Notification")
+                    .setMessage(message)
+                    .setPositiveButton("Ok", { dialog, which -> }).show()
+        }
+//        val hasRegistered = applicationContext.getSharedPreferences("myPref", Context.MODE_PRIVATE).getBoolean("device",false)
+//
+//       if(hasRegistered){
+           sendToken()
+//       }
+
+
     }
 
     private fun setupTabIcons() {
         tabs.getTabAt(0)!!.setIcon(R.mipmap.ic_action_home)
         tabs.getTabAt(1)!!.setIcon(R.mipmap.ic_action_profile)
+    }
+
+    private fun sendToken(){
+        val deviceURL = "https://gentle-atoll-11837.herokuapp.com/api/userdevice"
+        val sharedPreferences = applicationContext.getSharedPreferences("myPref", Context.MODE_PRIVATE).getString("myToken","")
+        var jsonRequest = object  : StringRequest(Request.Method.POST, deviceURL,  object : Response.Listener<String>{
+            override fun onResponse(response: String) {
+                applicationContext.getSharedPreferences("myPref", Context.MODE_PRIVATE).edit().putBoolean("device",true).commit()
+                Log.d("debug","API SENT")
+            }
+
+        }, object : Response.ErrorListener{
+            override fun onErrorResponse(error: VolleyError) {
+                Log.d("debug",FirebaseInstanceId.getInstance().getToken()!!)
+                Log.d("Debug", error.toString())
+            }
+
+        }){
+            @Throws(AuthFailureError::class)
+            override fun getHeaders():Map<String,String>{
+                val headers = HashMap<String, String>()
+                headers.put("Authorization", "Bearer "+sharedPreferences)
+                return headers
+            }
+
+                @Throws(AuthFailureError::class)
+                override fun getParams(): Map<String, String> {
+                    val params = java.util.HashMap<String, String>()
+//                params.put("profilepic", profileHolder)
+                    params.put("device_id", FirebaseInstanceId.getInstance().getToken()!!)
+
+                    return params
+                }
+        }
+
+        val requestVolley = Volley.newRequestQueue(applicationContext)
+        requestVolley.add(jsonRequest)
     }
 }
 
